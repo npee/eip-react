656eff5c03bd16aecca455eda1363801d2a57d7a
MyeongchanJin <jmcchan@naver.com>
2020-07-07 17:41:35 +0900

    withStyles을 이용하여 스타일 적용

--- src/component/quiz/AddQuizComponent.jsx
+++ src/component/quiz/AddQuizComponent.jsx
@@ -6,10 +6,29 @@ import Button from '@material-ui/core/Button';
 import Typography from "@material-ui/core/Typography";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import { withStyles } from '@material-ui/core/styles';
+import Container from "@material-ui/core/Container";
+import FormGroup from "@material-ui/core/FormGroup";
 
 // eslint-disable-next-line no-unused-vars
 class Quiz { quizId; year; nth; question; image; isCorrect; createdDate; modifiedDate; }
 console.log(Quiz);
+
+const useStyles = () => ({
+    typoGraphy: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    select: {
+        margin: 6,
+        minWidth: 120
+    },
+    textField: {
+        margin: 6,
+    }
+});
 
 class AddQuizComponent extends Component {
 
@@ -67,39 +86,43 @@ class AddQuizComponent extends Component {
     // TODO: 선택지(보기) 넣어야 함
     render() {
         const nths = ['1st', '2nd', '3rd', '1st+2nd'];
-        let years = [];
-        for (let y = 2020; y > 2000; y--) {
-            years.push(y);
+        const years = () => {
+            let yearList = [];
+            for (let y = 2020; y > 2000; y--) {
+                yearList.push(y);
+            }
+            return yearList;
         }
 
+
+        const classes = useStyles();
+
         return (
-            <div>
-                <Typography variant="h4" style={style}>퀴즈 등록</Typography>
+            <Container maxWidth="sm">
+                <Typography variant="h4" style={classes.typoGraphy}>퀴즈 등록</Typography>
                 <Button variant="contained" color="primary" onClick={this.home}>초기 화면으로</Button>
-                <form style={formContainer}>
-                    <Select name="year" value={this.state.year} onChange={this.handleChange}>{this.setItems(years)}</Select>
-                    <Select name="nth" value={this.state.nth} onChange={this.handleChange}>{this.setItems(nths)}</Select>
-                    <TextField type="text" name="question" placeholder="문제를 입력해주세요"
-                               fullWidth margin="normal" value={this.state.question} onChange={this.handleChange} />
-                    <TextField type="text" name="image" placeholder="이미지가 있으면 등록해주세요"
-                               fullWidth margin="normal" value={this.state.image} onChange={this.handleChange} />
-                    <TextField type="text" name="isCorrect" placeholder="true or false"
-                               fullWidth margin="normal" value={this.state.isCorrect} onChange={this.handleChange} />
-                    <Button variant="contained" color="primary" onClick={this.saveQuiz}>저장</Button>
-                </form>
-            </div>
+                <FormGroup row>
+                    <FormControl style={classes.select}>
+                        <InputLabel id="year-list">연도</InputLabel>
+                        <Select labelId="year-lists" name="year" value={this.state.year} onChange={this.handleChange}>{this.setItems(years())}</Select>
+                    </FormControl>
+                    <FormControl style={classes.select}>
+                        <InputLabel id="nth-list">회차</InputLabel>
+                        <Select labelId="nth-list" name="nth" value={this.state.nth} onChange={this.handleChange}>{this.setItems(nths)}</Select>
+                    </FormControl>
+                </FormGroup>
+                <FormGroup>
+                    <TextField required label="문제" type="text" name="question" placeholder="문제를 입력해주세요"
+                               fullWidth margin="normal" style={classes.textField} value={this.state.question} onChange={this.handleChange} />
+                    <TextField label="이미지" type="text" name="image" placeholder="이미지가 있으면 등록해주세요"
+                               fullWidth margin="normal" style={classes.textField} value={this.state.image} onChange={this.handleChange} />
+                    <TextField label="정답 여부" type="text" name="isCorrect" placeholder="true or false"
+                               fullWidth margin="normal" style={classes.textField} value={this.state.isCorrect} onChange={this.handleChange} />
+                </FormGroup>
+                <Button variant="contained" color="primary" onClick={this.saveQuiz}>저장</Button>
+            </Container>
         );
     }
 }
 
-const formContainer = {
-    display: 'flex',
-    flexFlow: 'row wrap'
-}
-
-const style = {
-    display: 'flex',
-    justifyContent: 'center'
-}
-
-export default AddQuizComponent;
+export default withStyles(useStyles)(AddQuizComponent);
